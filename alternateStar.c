0fdc788ba39328c5ed5dd9054383388f479917dd
nirman12 <nirmankhanal477@gmail.com>
2024-12-07 22:14:27 +0545

    files added

---
+++ alternateStar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i++){
+        if (i%2!=0){
+            printf("*****\n");
+        }
+        else{
+            printf("#####\n");
+        }
+    }
+    return 0;
+}
+
+
+//I do not think this si the correct way to do this but this is what i could think of
